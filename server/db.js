d51af864da664d0597f85724b931f76d4e7c82b2
Darien Alejandro Verdugo Reyna <L20303022@puertopenasco.tecnm.mx>
2023-03-06 15:46:19 -0700

    Routes

--- server/db.js
+++ server/db.js
@@ -1,5 +1,9 @@
 import {createPool} from 'mysql2/promise';
 
-export const pool = createPool({
-    
+export const pool = createPool({ 
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'itspp',
+  database: 'itspp-toolsdb',
 });
---
+++ server/routes/index.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express"
+import { pool } from "../db.js"
+
+const router = Router();
+
+router.get('/ping', (req, res) => {
+
+});
