01fef77ac691b3d6cf2cd9b794d3ff190c36c966
Darien Alejandro Verdugo Reyna <L20303022@puertopenasco.tecnm.mx>
2023-03-10 21:06:15 -0700

    cambios

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { PORT } from "./config.js";
 
-import indexRoutes from "./routes/index.routes.js";
+import indexRoutes from "./routes/index.routes.js"
 
 const app = express();
 
