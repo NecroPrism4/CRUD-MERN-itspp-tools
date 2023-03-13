b811081a0ebb6710f57ec33b1b47e15539032b2c
Darien Alejandro Verdugo Reyna <alexhid14@gmail.com>
2023-03-12 21:10:36 -0700

    Cambio otra vez

--- server/index.js
+++ server/index.js
@@ -2,10 +2,14 @@ import express from 'express';
 import { PORT } from './config.js';
 
 import indexRoutes from './routes/index.routes.js';
+import industrialRoutes from './routes/industrial.routes.js';
 
 const app = express();
 
+app.use(express.json());
+
 app.use(indexRoutes);
+app.use(industrialRoutes);
 
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
