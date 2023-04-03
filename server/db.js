e34a66ddddb4f9e6d4c3574902db680770e0632b
Darien Alejandro Verdugo Reyna <alexhid14@gmail.com>
2023-04-03 14:56:53 -0700

    Bases del proyecto

--- server/db.js
+++
@@ -1,9 +0,0 @@
-import { createPool } from 'mysql2/promise';
-
-export const pool = createPool({
-	host: '192.168.1.108',
-	port: 3306,
-	user: 'root',
-	password: 'itspp',
-	database: 'itspp-toolsdb',
-});
