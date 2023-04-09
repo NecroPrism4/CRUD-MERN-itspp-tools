76c4b9780d9cc20b935d23255c8f530968494868
Darien Alejandro Verdugo Reyna <alexhid14@gmail.com>
2023-04-08 19:57:48 -0700

    Changes to the css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { ThemeContextProvider } from './context/ThemeContext';
 import '../src/Style.css';
-import Login from '../src/pages/Login.jsx';
+import Login from './pages/Login/Login.jsx';
 import Home from '../src/pages/Home.jsx';
 
 import { Route, Routes } from 'react-router-dom';
--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,41 +1,42 @@
 import { useContext, useState } from 'react';
-import { ThemeContext } from '../context/ThemeContext';
-import DarkLightButton from '../components/MainPage/ButtonLightDark/DarkLightButton';
+import { ThemeContext } from '../../context/ThemeContext';
+import DarkLightButton from '../../components/MainPage/ButtonLightDark/DarkLightButton';
+import style from './Login.module.css';
 
 function Login() {
 	const { theme } = useContext(ThemeContext);
 
 	return (
 		<>
-			<div className='container' data-theme={theme}>
-				<div className='design'>
+			<div className={style.Container} data-theme={theme}>
+				<div className={style.Design}>
 					Design Area
 					<DarkLightButton></DarkLightButton>
-					<div className='svg-container'>
+					<div className={style.SvgContainer}>
 						<object
 							type='image/svg+xml'
 							data='/src/assets/Line1.svg'
 							width='100%'
 							height='100%'
-							className='svg-content'
+							className={style.SvgContent}
 						></object>
 					</div>
-					<div className='svg-container'>
+					<div className={style.SvgContainer}>
 						<object
 							type='image/svg+xml'
 							data='/src/assets/Line2.svg'
 							width='100%'
 							height='100%'
-							className='svg-content'
+							className={style.SvgContent}
 						></object>
 					</div>
-					<div className='svg-container'>
+					<div className={style.SvgContainer}>
 						<object
 							type='image/svg+xml'
 							data='/src/assets/Line3.svg'
 							width='100%'
 							height='100%'
-							className='svg-content'
+							className={style.SvgContent}
 						></object>
 					</div>
 				</div>
