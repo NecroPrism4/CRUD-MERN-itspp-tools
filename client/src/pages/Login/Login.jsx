5eedac5443b3bf2a143ec881c0733ab16ef2a251
Darien Alejandro Verdugo Reyna <alexhid14@gmail.com>
2023-04-10 16:59:22 -0700

    Logo Working!!!

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -10,55 +10,54 @@ function Login() {
 		<>
 			<div className='LoginPrimaryContainer' data-theme={theme}>
 				<div className='LoginDesignContainer'>
-					Design Area
 					<div className='LoginLogoContainer'>
-						<div className='SvgContainer'>
-							<object width='100%' height='100%' className='SvgContent'>
-								<svg
-									className='LoginLine1'
-									width='251'
-									height='105'
-									viewBox='0 0 251 105'
-									xmlns='http://www.w3.org/2000/svg'
-								>
-									<path d='M63.5708 63.4374C58.3479 46.5537 40.4269 37.1007 23.5432 42.3237C6.65944 47.5466 -2.79351 65.4676 2.42942 82.3513C7.65234 99.235 25.5733 108.688 42.457 103.465C59.3408 98.2421 68.7937 80.3211 63.5708 63.4374ZM246.616 0.532433L31.2269 67.1623L34.7733 78.6263L250.162 11.9964L246.616 0.532433Z' />
-								</svg>
-							</object>
-						</div>
-						<div className='SvgContainer'>
-							<object width='100%' height='100%' className='SvgContent'>
-								<svg
-									width='241'
-									height='284'
-									viewBox='0 0 241 284'
-									xmlns='http://www.w3.org/2000/svg'
-								>
+						<svg
+							width='629'
+							height='537'
+							viewBox='0 0 629 537'
+							xmlns='http://www.w3.org/2000/svg'
+						>
+							<g id='LogoApp '>
+								<g id='logo-lines'>
 									<path
-										className='LoginLine2'
-										d='M229.203 7.31885C215.572 -3.92989 195.403 -1.99867 184.154 11.6324C172.905 25.2634 174.836 45.4324 188.467 56.6811C202.098 67.9299 222.267 65.9987 233.516 52.3676C244.765 38.7366 242.834 18.5676 229.203 7.31885ZM204.207 28.1811L0.146059 275.459L9.40149 283.097L213.463 35.8189L204.207 28.1811Z'
+										id='line1'
+										d='M80.5708 354.437C75.3479 337.554 57.4269 328.101 40.5432 333.324C23.6594 338.547 14.2065 356.468 19.4294 373.351C24.6523 390.235 42.5733 399.688 59.457 394.465C76.3408 389.242 85.7937 371.321 80.5708 354.437ZM263.616 291.532L48.2269 358.162L51.7733 369.626L267.162 302.996L263.616 291.532Z'
 									/>
-								</svg>
-							</object>
-						</div>
-						<div className='SvgContainer'>
-							<object width='100%' height='100%' className='SvgContent'>
-								<svg
-									width='240'
-									height='227'
-									viewBox='0 0 240 227'
-									xmlns='http://www.w3.org/2000/svg'
-								>
 									<path
-										className='LoginLine3'
-										d='M230.946 216.624C243.024 203.721 242.355 183.471 229.452 171.394C216.55 159.316 196.3 159.985 184.222 172.887C172.145 185.79 172.814 206.04 185.716 218.117C198.618 230.195 218.869 229.526 230.946 216.624ZM211.685 190.375L8.50731 0.188082L0.306714 8.9488L203.484 199.136L211.685 190.375Z'
+										id='line2'
+										d='M539.202 7.31885C525.571 -3.92989 505.402 -1.99867 494.154 11.6324C482.905 25.2634 484.836 45.4324 498.467 56.6811C512.098 67.9299 532.267 65.9987 543.516 52.3676C554.765 38.7366 552.834 18.5676 539.202 7.31885ZM514.207 28.1811L310.146 275.459L319.401 283.097L523.463 35.8189L514.207 28.1811Z'
 									/>
-								</svg>
-							</object>
-						</div>
+									<path
+										id='line3'
+										d='M535.946 526.624C548.024 513.721 547.355 493.471 534.452 481.394C521.55 469.316 501.3 469.985 489.222 482.887C477.145 495.79 477.813 516.04 490.716 528.117C503.618 540.195 523.869 539.526 535.946 526.624ZM516.684 500.375L313.507 310.188L305.307 318.949L508.484 509.136L516.684 500.375Z'
+									/>
+								</g>
+								<g id='Text-Logo'>
+									<text fontSize='128' fontWeight='500' letterSpacing='0em'>
+										<tspan x='0' y='321.888'>
+											Gest
+										</tspan>
+										<tspan x='330.375' y='321.888'>
+											r de{' '}
+										</tspan>
+										<tspan x='0' y='421.888'>
+											laboratorio
+										</tspan>
+									</text>
+									<text
+										id='o'
+										fontSize='128'
+										fontWeight='500'
+										letterSpacing='0em'
+									>
+										<tspan x='258.5' y='321.888'>
+											o
+										</tspan>
+									</text>
+								</g>
+							</g>
+						</svg>
 					</div>
-					<h1 className='AppTitle'>
-						Gest<mark>o</mark>r de laboratorio
-					</h1>
 				</div>
 				<div className='LoginContainer'>
 					login area
