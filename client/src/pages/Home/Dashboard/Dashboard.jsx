9b4bd56e8dfeecfef5697db5935f7d0868c82bd7
Darien Alejandro Verdugo Reyna <L20303022@puertopenasco.tecnm.mx>
2023-05-24 09:18:50 -0700

    satrting the dashboard

--- client/src/pages/Home/Dashboard/Dashboard.jsx
+++ client/src/pages/Home/Dashboard/Dashboard.jsx
@@ -1,21 +1,53 @@
 import './Dashboard.css';
 import { useEffect, useContext } from 'react';
+import { SectionContext } from '../../../context/SectionContext';
 
 import Loading from '../../../components/HomePage/MainContainer/Loading/Loading.jsx';
 
-import { SectionContext } from '../../../context/SectionContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faMoon,
+	faSun,
+	faHandHoldingHand,
+} from '@fortawesome/free-solid-svg-icons';
 
 function Dashboard() {
 	const { handleTitle } = useContext(SectionContext);
 	useEffect(() => {
-		handleTitle('Dashboard');
+		handleTitle('Bienvenid@');
 	}, []);
 
 	return (
-		<div className='HomeChildContainer'>
-			<h1>Dashboard</h1>
+		<div className='HomeChildContainer Dashboard'>
+			<div className='DashboardContainer'>
+				<div className='ActionsContainer'>
+					<div className=''>
+						<button className='RoundedRect ImageButton'>
+							<div>
+								<FontAwesomeIcon
+									className='Icon'
+									icon={faHandHoldingHand}
+								></FontAwesomeIcon>
+								<img src='https://www.gob.mx/cms/uploads/article/main_image/104190/imagenes-07.jpg' />
+								<div>
+									<h4>Registrar Prestamo</h4>
+								</div>
+							</div>
+						</button>
+						<button className='RoundedRect ImageButton'>
+							<div>
+								<img src='https://www.gob.mx/cms/uploads/article/main_image/104190/imagenes-07.jpg' />
+								<div>
+									<h4>Devolución de prestamo</h4>
+								</div>
+							</div>
+						</button>
+					</div>
+					<div></div>
+				</div>
 
-			<Loading></Loading>
+				<Loading></Loading>
+			</div>
 		</div>
 	);
 }
